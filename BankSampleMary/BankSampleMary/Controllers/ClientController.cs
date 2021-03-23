9097aae0cc6880a5c1c102c5009826b4bcc58818
Maria Victor <54823595+Maryvictor@users.noreply.github.com>
2021-03-23 07:17:18 -0300

    Add files via upload

---
+++ BankSampleMary/BankSampleMary/Controllers/ClientController.cs
@@ -0,0 +1,45 @@
+﻿using BankSampleMary.Model;
+using BankSampleMary.Model.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSampleMary.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClienteController : Controller
+    {
+        private ClienteRepository Repository = new ClienteRepository();
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await this.Repository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            return Ok(await this.Repository.Get(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Cliente model)
+        {
+            await this.Repository.Save(model);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(Guid id, [FromBody] Cliente model)
+        {
+            await this.Repository.Update(id, model);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await this.Repository.Remove(id);
+            return Ok();
+        }
+    }
+}
---
+++ BankSampleMary/BankSampleMary/Model/Repository/ClientRepository.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankSampleMary.Model.Repository
+{
+    public class ClienteRepository
+    {
+        public string Endpoint = @"";
+        public string Key = "";
+        public string DatabaseId = "BankSampleMary";
+        public string CollectionId = "ClienteCollection";
+        public DocumentClient DocumentClient;
+        public ClienteRepository()
+        {
+            this.DocumentClient = new DocumentClient(new Uri(Endpoint)
+                                                    , Key);
+            DocumentClient.CreateDatabaseIfNotExistsAsync(
+            new Microsoft.Azure.Documents.Database() { Id = DatabaseId })
+           .Wait();
+
+            DocumentClient.CreateDocumentCollectionIfNotExistsAsync(UriFactory.CreateDatabaseUri(DatabaseId),
+                                                                    new DocumentCollection() { Id = CollectionId },
+                                                                    new RequestOptions()
+                                                                    {
+                                                                        OfferThroughput = 1000
+                                                                    })
+            .Wait();
+        }
+        public async Task Save(Cliente model)
+        {
+            await DocumentClient.CreateDocumentAsync(
+            UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), model);
+        }
+        public async Task Remove(Guid id)
+        {
+            await DocumentClient.DeleteDocumentAsync(
+            UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id.ToString()));
+        }
+        public async Task Update(Guid id, Cliente model)
+        {
+            await DocumentClient.ReplaceDocumentAsync(
+            UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id.ToString()), model);
+        }
+        public async Task<Cliente> Get(Guid id)
+        {
+            try
+            {
+                Document doc = await DocumentClient.ReadDocumentAsync(
+                UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id.ToString()));
+
+                return JsonConvert.DeserializeObject<Cliente>(doc.ToString());
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    return null;
+                else
+                    throw;
+            }
+        }
+        public async Task<IEnumerable<Cliente>> GetAll()
+        {
+            var documents = DocumentClient.CreateDocumentQuery<Cliente>
+                            (UriFactory.CreateDocumentCollectionUri(
+                             DatabaseId, CollectionId),
+                             new FeedOptions { MaxItemCount = -1 })
+                            .AsDocumentQuery();
+            List<Cliente> result = new List<Cliente>();
+            while (documents.HasMoreResults)
+                result.AddRange(await documents.ExecuteNextAsync<Cliente>());
+            return result;
+        }
+    }
+}
